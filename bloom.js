48588af2d1535b31a1a12306f0c84b7bce85f1ec
MengiCodes <mengimagheed7@gmail.com>
2025-10-24 17:49:51 +0300

    apply responsive design and header navigation on pages

--- bloom.js
+++ bloom.js
@@ -1,12 +1,15 @@
 const menuHumburger = document.querySelector("#menu");
+const navigation = document.querySelector("#navigation")
 menuHumburger.addEventListener("click", function(){
   menuHumburger.classList.toggle("show");
+  navigation.classList.toggle("show")
 
 })
 const icon_btn = document.querySelector(".icon");
+const problems = document.querySelector(".problems")
 const problem_hide = document.querySelector("#problem-hide");
 
-icon_btn.addEventListener("click", function (e) {
+problems.addEventListener("click", function (e) {
   e.preventDefault();
   problem_hide.classList.toggle("unhide");
   icon_btn.classList.toggle("rotate");
