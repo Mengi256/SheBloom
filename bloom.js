5aca91fa56207cc1a3f9da6488480e288e10e619
MengiCodes <mengimagheed7@gmail.com>
2025-10-26 13:48:43 +0300

    fix pages

--- bloom.js
+++ bloom.js
@@ -1,45 +1,34 @@
-// const menuHumburger = document.querySelector("#menu");
-// const navigation = document.querySelector("#navigation")
-// menuHumburger.addEventListener("click", function(){
-//   menuHumburger.classList.toggle("show");
-//   navigation.classList.toggle("show")
 
-// })
-// const icon_btn = document.querySelector(".icon");
-// const problems = document.querySelector(".problems")
-// const problem_hide = document.querySelector("#problem-hide");
-
-// problems.addEventListener("click", function (e) {
-//   e.preventDefault();
-//   problem_hide.classList.toggle("unhide");
-//   icon_btn.classList.toggle("rotate");
-// });
-
-// const year = document.querySelector("#year");
-//   const today = new Date();
-// year.textContent = today.getFullYear()
 
 const menuHumburger = document.querySelector("#menu");
 const navigation = document.querySelector("#navigation");
+const header_nav = document.querySelector(".nav");
 
-if (menuHumburger && navigation) {
+if (menuHumburger && navigation && header_nav) {
   menuHumburger.addEventListener("click", function () {
     menuHumburger.classList.toggle("show");
     navigation.classList.toggle("show");
+    header_nav.classList.toggle("show")
   });
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  const logo = document.querySelector(".logo");
-  const header_btn = document.querySelector(".header-btn")
-  if (logo && header_btn) {
-    // Delay slightly for smoother effect
-    setTimeout(() => {
-      logo.classList.add("slide-in");
-      header_btn.classList.add("slide-in")
-    }, 100);
-  }
-});
+
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const logo = document.querySelector(".logo");
+//   const header_btn = document.querySelector(".header-btn")
+//   if (logo && header_btn) {
+   
+//     setTimeout(() => {
+//       logo.classList.add("slide-in");
+//       header_btn.classList.add("slide-in")
+//     }, 100);
+//   }
+// });
 
 
 
@@ -52,6 +41,19 @@ if (problems && problem_hide && icon_btn) {
     e.preventDefault();
     problem_hide.classList.toggle("unhide");
     icon_btn.classList.toggle("rotate");
+  });
+}
+
+
+const quick_icon = document.querySelector(".quick-icon");
+
+const links_box = document.querySelector(".links-box");
+
+if (quick_icon && links_box) {
+  quick_icon.addEventListener("click", function (e) {
+    e.preventDefault();
+    links_box.classList.toggle("unhide");
+    quick_icon.classList.toggle("rotate");
   });
 }
 
